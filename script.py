50ef159c71acbf13e1333b12538e81c8101df93f
Sten6130 <stennierop@outlook.com>
2022-04-01 18:47:30 +0200

    variables and if else

---
+++ script.py
@@ -0,0 +1,11 @@
+print("Hello, World!")
+name = "Sten"
+age = 21
+age2 = 20
+print("my name is", name, "and i'm", age ,"years old")
+
+if age > age2:
+    print("Sten is 17 and older")
+else:
+    print("age2 is older than Sten")
+  
