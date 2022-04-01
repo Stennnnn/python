4ec5e0ce3486bb87f1ab9d8c2e81e9a445f01eb7
Sten6130 <stennierop@outlook.com>
2022-04-01 18:56:54 +0200

    variables more explained

---
+++ variables.py
@@ -0,0 +1,22 @@
+sten = 17 #integer
+name = "Sten" #string
+print(sten)
+print(name)
+
+a = str(2) #specify types by casting
+b = int(2)
+c = float(2)
+
+print(a)
+print(b)
+print(c)
+
+#strings can be specified by both single quotes and double quotes
+
+#variables are case sensitive
+
+#get variable data type
+x = 5
+y = "Sten"
+print(type(x))
+print(type(y))
